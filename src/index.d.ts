b342e75eac0b55e54e724f3f63dc314c4d8a8892
doinkythederp <mcclelll@icloud.com>
2021-04-24 15:40:55 -0700

    Reformat
    - Changed type from module
    - Removed src/cjs/
    - Removed ts/
    - Split index.ts into index.js and index.d.ts
    - Added more JSDoc comments

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,60 +1,111 @@
 /** The type of the progress bar */
-export declare type ProgressBarType = "bar" | "bar/spin" | "spin" | "percent" | "percent/spin" | "bar/percent" | "bar/percent/spin";
+export type ProgressBarType = "bar" | "bar/spin" | "spin" | "percent" | "percent/spin" | "bar/percent" | "bar/percent/spin";
+
+declare const AnsiCodes: {
+	clear: "\u001b[2K",
+	start: "\u001b[1000D"
+}
+
+declare const spinStates: {
+	0: "/",
+	1: "—",
+	2: "\\",
+	3: "—"
+}
+
 interface ProgressBarOptions {
-    type?: ProgressBarType;
-    label?: string;
-    start?: number;
-    max?: number;
-    length?: number;
+	type?: ProgressBarType,
+	label?: string,
+	start?: number,
+	max?: number,
+	length?: number
 }
-/**
- * Progress bar that goes in the console
- */
-export default class ProgressBar {
-    constructor(options?: ProgressBarOptions, out?: typeof process.stdout);
-    /** The length of the bar when using bar types */
-    barLength: number;
+
+declare class ProgressBar {
     /**
-     * Whether or not the progress bar has been rendered
-     * @protected
+     * Create a new ProgressBar
+     * Hidden by default, unless render or tick is called.
+     * @param options 
+     * @param out 
      */
-    rendered: boolean;
-    /** The type of this progress bar */
-    type: ProgressBarType;
-    /** The percent completion of the progress bar. */
-    percent: number;
+	constructor(options?: ProgressBarOptions, out?: typeof process.stdout);
+
+	/** The length of the bar when using bar types */
+	barLength: number;
+
+	/**
+	 * Whether or not the progress bar has been rendered
+	 * @protected
+	 */
+    protected rendered: boolean;
+
+	/** The type of this progress bar */
+	type: ProgressBarType;
+
+	/** The percent completion of the progress bar. */
+	percent: number;
+
+	/** 
+	 * The number `this.percent` has to get to before the bar is complete
+	 * @default 100
+	 */
+	max: number;
+
+	/** The write stream to log to */
+	out: typeof process.stdout;
+
+	/** The label of the progress bar */
+	label: string;
+
+	/** @protected */
+	protected _spinState: number;
+
+	/** Promise that resolves when the bar has finished (when `this.percent >= this.max`) */
+	finish: Promise<void>;
+
+	/** @private */
+	private _finish: Function;
+
+	/** Whether or not the progress bar has finished - if true, means that logging to the console is safe. */
+	isFinished: boolean;
+
+	/** @protected */
+	protected static _renderBar(out: typeof process.stdout, length: number, label: string, percent: number, max: number): void
+	/** @protected */
+	protected static _renderBarSpin(out: typeof process.stdout, length: number, label: string, percent: number, max: number, spinState: number): void
+
+	/** @protected */
+	protected static _renderSpin(out: typeof process.stdout, label: string, spinState: number): void
+
+	/** @protected */
+	protected static _renderPercent(out: typeof process.stdout, label: string, percent: number, max: number): void
+
+	/** @protected */
+	protected static _renderPercentSpin(out: typeof process.stdout, label: string, percent: number, max: number, spinState: number): void
+
+	/** @protected */
+	protected static _renderBarPercent(out: typeof process.stdout, length: number, label: string, percent: number, max: number): void
+
+	/** @protected */
+	protected static _renderBarPercentSpin(out: typeof process.stdout, length: number, label: string, percent: number, max: number, spinState: number): void
+
     /**
-     * The number `this.percent` has to get to before the bar is complete
-     * @default 100
+     * Render the progress bar
      */
-    max: number;
-    /** The write stream to log to */
-    out: typeof process.stdout;
-    /** The label of the progress bar */
-    label: string;
-    /** @protected */
-    _spinState: number;
-    /** Promise that resolves when the bar has finished (when `this.percent >= this.max`) */
-    finish: Promise<void>;
-    /** @private */
-    _finish: Function;
-    /** Whether or not the progress bar has finished - if true, means that logging to the console is safe. */
-    isFinished: boolean;
-    /** @protected */
-    static _renderBar(out: typeof process.stdout, length: number, label: string, percent: number, max: number): void;
-    /** @protected */
-    static _renderBarSpin(out: typeof process.stdout, length: number, label: string, percent: number, max: number, spinState: number): void;
-    /** @protected */
-    static _renderSpin(out: typeof process.stdout, label: string, spinState: number): void;
-    /** @protected */
-    static _renderPercent(out: typeof process.stdout, label: string, percent: number, max: number): void;
-    /** @protected */
-    static _renderPercentSpin(out: typeof process.stdout, label: string, percent: number, max: number, spinState: number): void;
-    /** @protected */
-    static _renderBarPercent(out: typeof process.stdout, length: number, label: string, percent: number, max: number): void;
-    /** @protected */
-    static _renderBarPercentSpin(out: typeof process.stdout, length: number, label: string, percent: number, max: number, spinState: number): void;
-    render(): void;
-    tick(percent?: number): this;
+	render(): this
+
+    /**
+     * Update the progress bar.
+     * @param percent - Change the completion percent by this much
+     */
+	tick(percent: number): this
+
+    /** Set the completion to 100% */
+	setFinished(): void
 }
-export {};
+
+export default ProgressBar;
+
+declare function getPercent(current: number, max: number): number
+
+declare function createBar(current: number, max: number, length: number): string
--- src/index.js
+++ src/index.js
@@ -1,125 +1,255 @@
-var AnsiCodes;
-(function (AnsiCodes) {
-    AnsiCodes["clear"] = "\u001B[2K";
-    AnsiCodes["start"] = "\u001B[1000D";
-})(AnsiCodes || (AnsiCodes = {}));
+/**
+ * @module @doinkythederp/progress-bar
+ */
+
+const AnsiCodes = {
+  clear: "\u001b[2K",
+  start: "\u001b[1000D",
+};
+
 const spinStates = {
-    0: "/",
-    1: "—",
-    2: "\\",
-    3: "—"
+  0: "/",
+  1: "—",
+  2: "\\",
+  3: "—",
+};
+
+module.exports = class ProgressBar {
+  constructor(options, out = process.stdout) {
+    options = options ?? {};
+    (this.type = options.type ?? "bar"),
+      (this.out = out),
+      (this.percent = options.start ?? 0),
+      (this.max = options.max ?? 100),
+      (this.label = options.label ?? ""),
+      (this._spinState = 0),
+      (this.isFinished = false),
+      (this.rendered = false),
+      (this.barLength = options.length ?? 20);
+
+    this.finish = new Promise((resolve) => {
+      this._finish = resolve;
+    });
+  }
+
+  barLength;
+
+  rendered;
+
+  type;
+
+  percent;
+
+  max;
+
+  out;
+
+  label;
+
+  _spinState;
+
+  finish;
+
+  _finish;
+
+  isFinished;
+
+  static _renderBar(out, length, label, percent, max) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}[${createBar(percent, max, length)}]`.substr(
+        0,
+        out.columns
+      )
+    );
+  }
+
+  /** @protected */
+  static _renderBarSpin(out, length, label, percent, max, spinState) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}[${createBar(percent, max, length)}]${
+        percent !== max ? " " + spinStates[spinState] : " ✓"
+      }`.substr(0, out.columns)
+    );
+  }
+
+  /** @protected */
+  static _renderSpin(out, label, spinState) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}${spinStates[spinState]}`.substr(
+        0,
+        out.columns
+      )
+    );
+  }
+
+  /** @protected */
+  static _renderPercent(out, label, percent, max) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}${getPercent(percent, max)}%`.substr(
+        0,
+        out.columns
+      )
+    );
+  }
+
+  /** @protected */
+  static _renderPercentSpin(out, label, percent, max, spinState) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}${getPercent(percent, max)}%${
+        percent !== max ? " " + spinStates[spinState] : " ✓"
+      }`.substr(0, out.columns)
+    );
+  }
+
+  /** @protected */
+  static _renderBarPercent(out, length, label, percent, max) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}[${createBar(
+        percent,
+        max,
+        length
+      )}] ${getPercent(percent, max)}%`.substr(0, out.columns)
+    );
+  }
+
+  /** @protected */
+  static _renderBarPercentSpin(out, length, label, percent, max, spinState) {
+    out.write(AnsiCodes.clear);
+    out.write(AnsiCodes.start);
+    out.write(
+      `${label ? label + " " : ""}[${createBar(
+        percent,
+        max,
+        length
+      )}] ${getPercent(percent, max)}%${
+        percent !== max ? " " + spinStates[spinState] : " ✓"
+      }`.substr(0, out.columns)
+    );
+  }
+
+  render() {
+    switch (this.type) {
+      case "bar":
+        ProgressBar._renderBar(
+          this.out,
+          this.barLength,
+          this.label,
+          this.percent,
+          this.max
+        );
+        break;
+      case "bar/spin":
+        ProgressBar._renderBarSpin(
+          this.out,
+          this.barLength,
+          this.label,
+          this.percent,
+          this.max,
+          this._spinState
+        );
+        break;
+      case "spin":
+        ProgressBar._renderSpin(this.out, this.label, this._spinState);
+        break;
+      case "percent":
+        ProgressBar._renderPercent(
+          this.out,
+          this.label,
+          this.percent,
+          this.max
+        );
+        break;
+      case "percent/spin":
+        ProgressBar._renderPercentSpin(
+          this.out,
+          this.label,
+          this.percent,
+          this.max,
+          this._spinState
+        );
+        break;
+      case "bar/percent":
+        ProgressBar._renderBarPercent(
+          this.out,
+          this.barLength,
+          this.label,
+          this.percent,
+          this.max
+        );
+        break;
+      case "bar/percent/spin":
+        ProgressBar._renderBarPercentSpin(
+          this.out,
+          this.barLength,
+          this.label,
+          this.percent,
+          this.max,
+          this._spinState
+        );
+        break;
+    }
+
+    return this;
+  }
+
+  tick(percent = 1) {
+    let doFinish = false;
+
+    ++this._spinState;
+    if (this._spinState > 3) this._spinState = 0;
+
+    this.percent += percent;
+    if (getPercent(this.percent, this.max) === 100) {
+      this.percent = this.max;
+
+      doFinish = true;
+    }
+    this.render();
+
+    if (doFinish) {
+      console.log();
+      if (!this.isFinished) this._finish();
+      this.isFinished = true;
+    }
+    return this;
+  }
+
+  setFinished() {
+    (this.max = 100), (this.percent = 100);
+    this.tick(0);
+  }
 };
 /**
- * Progress bar that goes in the console
+ * Returns a percent from a value and a maximum
+ * @param {number} current
+ * @param {number} max
  */
-export default class ProgressBar {
-    constructor(options, out = process.stdout) {
-        options = options ?? {};
-        this.type = options.type ?? "bar",
-            this.out = out, this.percent = options.start ?? 0,
-            this.max = options.max ?? 100,
-            this.label = options.label ?? "",
-            this._spinState = 0,
-            this.isFinished = false,
-            this.rendered = false,
-            this.barLength = options.length ?? 20;
-        this.finish = new Promise((resolve) => {
-            this._finish = resolve;
-        });
-    }
-    /** @protected */
-    static _renderBar(out, length, label, percent, max) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}[${createBar(percent, max, length)}]`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderBarSpin(out, length, label, percent, max, spinState) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}[${createBar(percent, max, length)}]${percent !== max ? " " + spinStates[spinState] : " ✓"}`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderSpin(out, label, spinState) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}${spinStates[spinState]}`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderPercent(out, label, percent, max) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}${getPercent(percent, max)}%`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderPercentSpin(out, label, percent, max, spinState) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}${getPercent(percent, max)}%${percent !== max ? " " + spinStates[spinState] : " ✓"}`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderBarPercent(out, length, label, percent, max) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}[${createBar(percent, max, length)}] ${getPercent(percent, max)}%`.substr(0, out.columns));
-    }
-    /** @protected */
-    static _renderBarPercentSpin(out, length, label, percent, max, spinState) {
-        out.write(AnsiCodes.clear);
-        out.write(AnsiCodes.start);
-        out.write(`${label ? label + " " : ""}[${createBar(percent, max, length)}] ${getPercent(percent, max)}%${percent !== max ? " " + spinStates[spinState] : " ✓"}`.substr(0, out.columns));
-    }
-    render() {
-        switch (this.type) {
-            case "bar":
-                ProgressBar._renderBar(this.out, this.barLength, this.label, this.percent, this.max);
-                break;
-            case "bar/spin":
-                ProgressBar._renderBarSpin(this.out, this.barLength, this.label, this.percent, this.max, this._spinState);
-                break;
-            case "spin":
-                ProgressBar._renderSpin(this.out, this.label, this._spinState);
-                break;
-            case "percent":
-                ProgressBar._renderPercent(this.out, this.label, this.percent, this.max);
-                break;
-            case "percent/spin":
-                ProgressBar._renderPercentSpin(this.out, this.label, this.percent, this.max, this._spinState);
-                break;
-            case "bar/percent":
-                ProgressBar._renderBarPercent(this.out, this.barLength, this.label, this.percent, this.max);
-                break;
-            case "bar/percent/spin":
-                ProgressBar._renderBarPercentSpin(this.out, this.barLength, this.label, this.percent, this.max, this._spinState);
-                break;
-        }
-    }
-    tick(percent = 1) {
-        let doFinish = false;
-        ++this._spinState;
-        if (this._spinState > 3)
-            this._spinState = 0;
-        this.percent += percent;
-        if (this.percent >= this.max) {
-            this.percent = this.max;
-            doFinish = true;
-        }
-        this.render();
-        if (doFinish) {
-            console.log();
-            if (!this.isFinished)
-                this._finish();
-            this.isFinished = true;
-        }
-        return this;
-    }
-}
 function getPercent(current, max) {
-    let percent = Math.floor(current / (max / 100));
-    if (isNaN(percent))
-        percent = 100;
-    return percent;
+  let percent = Math.floor(current / (max / 100));
+  if (isNaN(percent)) percent = 100;
+  return percent;
 }
+
+/**
+ * Creates a bar that is partly filled based on the inputs
+ * @param {number} current
+ * @param {number} max
+ * @param {number} length - The length of the bar
+ */
 function createBar(current, max, length) {
-    return `[${"#".repeat(Math.floor(current / (max / length)))}${" ".repeat(length - Math.floor(current / (max / length)))}]`;
+  return `${"#".repeat((getPercent(current, max) / 100) * length)}${" ".repeat(
+    length - (getPercent(current, max) / 100) * length
+  )}`;
 }
